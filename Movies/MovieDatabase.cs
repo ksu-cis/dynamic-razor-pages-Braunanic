63d99bf42d93f821dfc31c6147efcd2fb5c74bda
nick braun <nickb7007@gmail.com>
2020-04-22 22:57:24 -0500

    F

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -24,11 +24,80 @@ namespace Movies
                 string json = file.ReadToEnd();
                 movies = JsonConvert.DeserializeObject<List<Movie>>(json);
             }
+            HashSet<string> genreSet = new HashSet<string>();
+            foreach (Movie movie in movies)
+            {
+                if (movie.MajorGenre != null)
+                {
+                    genreSet.Add(movie.MajorGenre);
+                }
+            }
+            genres = genreSet.ToArray();
         }
 
         /// <summary>
         /// Gets all the movies in the database
         /// </summary>
         public static IEnumerable<Movie> All { get { return movies; } }
+
+        /// <summary>
+        /// Searches the database for matching movies
+        /// </summary>
+        /// <param name="terms">The terms to search for</param>
+        /// <returns>A collection of movies</returns>
+        public static IEnumerable<Movie> Search(string terms)
+        {
+            List<Movie> results = new List<Movie>();
+            if (terms == null) return All;
+            foreach (Movie movie in All)
+            {
+                if (movie.Title != null && movie.Title.Contains(terms, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Gets the possible MPAARatings
+        /// </summary>
+        public static string[] MPAARatings
+        {
+            get => new string[]
+            {
+            "G",
+            "PG",
+            "PG-13",
+            "R",
+            "NC-17"
+            };
+        }
+
+        /// <summary>
+        /// Filters the provided collection of movies
+        /// </summary>
+        /// <param name="movies">The collection of movies to filter</param>
+        /// <param name="ratings">The ratings to include</param>
+        /// <returns>A collection containing only movies that match the filter</returns>
+        public static IEnumerable<Movie> FilterByMPAARating(IEnumerable<Movie> movies, IEnumerable<string> ratings)
+        {
+            if (ratings == null || ratings.Count() == 0) return movies;
+            List<Movie> results = new List<Movie>();
+            foreach (Movie movie in movies)
+            {
+                if (movie.MPAARating != null && ratings.Contains(movie.MPAARating))
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
+
+        private static string[] genres;
+        /// <summary>
+        /// Gets the movie genres represented in the database 
+        /// </summary>
+        public static string[] Genres => genres;
     }
 }
